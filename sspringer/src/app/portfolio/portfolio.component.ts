c6c6d37a9726c993aecf1d2997872d785bbb683f
sebspringer <sebspringer@gmail.com>
2018-02-08 15:33:22 +0100

    projects

--- sspringer/src/app/portfolio/portfolio.component.ts
+++ sspringer/src/app/portfolio/portfolio.component.ts
@@ -7,10 +7,15 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+  rule: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+
+    this.http.get('assets/projects.json').subscribe(data => {
+      this.rule = data;
+    });
   }
 
 }
